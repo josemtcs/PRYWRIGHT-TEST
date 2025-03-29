ff529586eb2cd1efb74510604e116fc47555321b
josem <jose20030707@gmail.com>
2025-03-28 19:35:57 -0500

    Quitada de credenciales

--- test/login.spec.js
+++ test/login.spec.js
@@ -4,8 +4,8 @@ test('login', async ({ page }) => {
 
   await page.goto('https://virtual.tdea.edu.co/login/index.php ');
 
-  await page.fill('#username','jose.taborda32');
-  await page.fill('#password','SpOnGa556');
+  await page.fill('#username','');
+  await page.fill('#password','');
   await page.click('button[id="loginbtn"]');
   await expect(page).toHaveURL('https://virtual.tdea.edu.co/my/courses.php');
 });
